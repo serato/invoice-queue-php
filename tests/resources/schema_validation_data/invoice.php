2ff6567abb1565cd9c358be6bc89c0063a38abfd
Chris Deighton <chris@serato.com>
2020-01-22 09:49:45 +1300

    Adds more test case data

--- tests/resources/schema_validation_data/invoice.php
+++ tests/resources/schema_validation_data/invoice.php
@@ -507,6 +507,45 @@ return [
     ],
     [
         null,
+        false,
+        [
+            'source' => 'SwsEc',
+            'invoice_id' => 'A STRING VAL',
+            'invoice_date' => '2020-01-21T08:54:09Z',
+            'transaction_reference' => 'A STRING VAL',
+            'moneyworks_debtor_code' => 'WEBC001',
+            'subscription_id' => 'A STRING VAL',
+            'currency' => 'USD',
+            'gross_amount' => 0,
+            'billing_address' => [
+                'company_name' => 'Company Inc',
+                'person_name' => 'Jo Bloggs',
+                'address_1' => '123 Street Road',
+                'address_2' => 'Suburbia',
+                'address_3' => 'The Stixx',
+                'city' => 'Townsville',
+                'region' => 'Statey',
+                'post_code' => '90210',
+                'country_iso' => 'NZ'
+            ],
+            'items' => [
+                # Valid item
+                [
+                    'sku' => 'SKU1',
+                    'quantity' => 1,
+                    'amount_gross' => 0,
+                    'amount_tax' => 0,
+                    'amount_net' => 0,
+                    'unit_price' => 0,
+                    'tax_code' => 'V'
+                ],
+                # Invalid item
+                []
+            ]
+        ]
+    ],
+    [
+        null,
         true,
         [
             'source' => 'SwsEc',
@@ -536,6 +575,51 @@ return [
                     'amount_tax' => 0,
                     'amount_net' => 0,
                     'unit_price' => 0,
+                    'tax_code' => 'V'
+                ]
+            ]
+        ]
+    ],
+    [
+        null,
+        true,
+        [
+            'source' => 'SwsEc',
+            'invoice_id' => 'A STRING VAL',
+            'invoice_date' => '2020-01-21T08:54:09Z',
+            'transaction_reference' => 'A STRING VAL',
+            'moneyworks_debtor_code' => 'WEBC001',
+            'subscription_id' => 'A STRING VAL',
+            'currency' => 'USD',
+            'gross_amount' => 0,
+            'billing_address' => [
+                'company_name' => 'Company Inc',
+                'person_name' => 'Jo Bloggs',
+                'address_1' => '123 Street Road',
+                'address_2' => 'Suburbia',
+                'address_3' => 'The Stixx',
+                'city' => 'Townsville',
+                'region' => 'Statey',
+                'post_code' => '90210',
+                'country_iso' => 'NZ'
+            ],
+            'items' => [
+                [
+                    'sku' => 'SKU1',
+                    'quantity' => 1,
+                    'amount_gross' => 0,
+                    'amount_tax' => 0,
+                    'amount_net' => 0,
+                    'unit_price' => 0,
+                    'tax_code' => 'V'
+                ],
+                [
+                    'sku' => 'SKU2',
+                    'quantity' => 20,
+                    'amount_gross' => 21000,
+                    'amount_tax' => 1000,
+                    'amount_net' => 20000,
+                    'unit_price' => 1000,
                     'tax_code' => 'V'
                 ]
             ]
