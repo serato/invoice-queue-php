24c0dd06ac8ac82536a5b221428a843efb62dc70
Chris Deighton <chris@serato.com>
2020-01-23 12:13:30 +1300

    Implements queue sending methods

--- src/SqsQueue.php
+++ src/SqsQueue.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 namespace Serato\InvoiceQueue;
 
 use Aws\Sqs\SqsClient;
+use Aws\Result;
+use Aws\Exception\AwsException;
 use Aws\Sqs\Exception\SqsException;
+use Serato\InvoiceQueue\Exception\ValidationException;
+use Serato\InvoiceQueue\Exception\QueueSendException;
 use Exception;
 
 /**
@@ -14,6 +18,8 @@ use Exception;
  */
 class SqsQueue
 {
+    private const SEND_BATCH_SIZE = 10;
+
     /** @var SqsClient */
     private $sqsClient;
 
@@ -22,6 +28,9 @@ class SqsQueue
 
     /** @var string */
     private $queueUrl;
+
+    /** @var array */
+    private $messageBatch;
 
     /**
      * Constructs the object
@@ -39,6 +48,54 @@ class SqsQueue
         }
         # FIFO queue names MUST end in ".fifo"
         $this->queueName = 'InvoiceData-' . $queueEnv . '.fifo';
+    }
+
+    public function __destruct()
+    {
+        $this->sendMessageBatch();
+    }
+
+    /**
+     * Sends a single Invoice to the message queue. Returns an SQS message ID.
+     *
+     * @param Invoice $invoice
+     * @param InvoiceValidator|null $validator
+     * @return string
+     * @throws ValidationException
+     */
+    public function sendInvoice(Invoice $invoice, ?InvoiceValidator $validator = null): string
+    {
+        if ($validator === null) {
+            $validator = new InvoiceValidator;
+        }
+        if (!$validator->validateArray($invoice->getData())) {
+            throw new ValidationException($validator->getErrors());
+        }
+        try {
+            $result = $this->getSqsClient()->sendMessage($this->invoiceToSqsSendParams($invoice));
+            return $result['MessageId'];
+        } catch (AwsException $e) {
+            $this->throwQueueSendException($e);
+        }
+    }
+
+    /**
+     * Adds an invoice to a batch for future batch delivery to message queue.
+     *
+     * @param Invoice $invoice
+     * @param InvoiceValidator $validator
+     * @return void
+     * @throws ValidationException
+     */
+    public function sendInvoiceToBatch(Invoice $invoice, InvoiceValidator $validator): void
+    {
+        if (!$validator->validateArray($invoice->getData())) {
+            throw new ValidationException($validator->getErrors());
+        }
+        if (count($this->messageBatch) === self::SEND_BATCH_SIZE) {
+            $this->sendMessageBatch();
+        }
+        $this->messageBatch[] = $this->invoiceToSqsSendParams($invoice, (string)count($this->messageBatch));
     }
 
     /**
@@ -118,5 +175,88 @@ class SqsQueue
     public function getSqsClient(): SqsClient
     {
         return $this->sqsClient;
+    }
+
+    /**
+     * Converts an Invoice into a param array suitable for sending to an SQS queue.
+     *
+     * @param Invoice   $invoice
+     * @param string    $batchMessageId     An ID that is unique within a batch of messages
+     *                                      (required for batch operations)
+     * @return array
+     */
+    private function invoiceToSqsSendParams(Invoice $invoice, string $batchMessageId = null): array
+    {
+        return array_merge(
+            [
+                'MessageAttributes' => [
+                    'InvoiceSource' => [
+                        'DataType'      => 'String',
+                        'StringValue'   => $invoice->getSource()
+                    ],
+                    'InvoiceId' => [
+                        'DataType'      => 'String',
+                        'StringValue'   => $invoice->getInvoiceId()
+                    ]
+                ],
+                'MessageBody' => json_encode($invoice->getData()),
+                'MessageDeduplicationId' => $invoice->getInvoiceId(),
+                'MessageGroupId' => $invoice->getSource()
+            ],
+            // If message is NOT part of batch add the queue URL
+            // If it IS part of batch add the message ID
+            ($batchMessageId === null ?
+                ['QueueUrl' => $this->getQueueUrl()] :
+                ['Id' => $batchMessageId]
+            )
+        );
+    }
+
+    /**
+     * @return Result | null
+     */
+    private function sendMessageBatch()
+    {
+        if (count($this->messageBatch) > 0) {
+            try {
+                $result = $this->getSqsClient()->sendMessageBatch([
+                    'Entries'   => $this->messageBatch,
+                    'QueueUrl'  => $this->getQueueUrl()
+                ]);
+                $this->messageBatch = [];
+                return $result;
+            } catch (AwsException $e) {
+                $this->throwQueueSendException($e);
+            }
+        }
+    }
+    /**
+     * Throws a `QueueSendException` exception in response to catching an
+     * `AwsException` exception.
+     *
+     * Uses the`AwsException` instance to create a meaningful error message when
+     * throwing the `QueueSendException` exception.
+     *
+     * @throws QueueSendException
+     */
+    private function throwQueueSendException(AwsException $e)
+    {
+        $msg = 'Error sending message to SQS queue `' . $this->getQueueName() .
+                '`.' . PHP_EOL .
+                'The AWS SDK threw an exception with the following details:' . PHP_EOL .
+                'Exception class: ' . get_class($e) . PHP_EOL .
+                'Exception message: ' . $e->getMessage();
+
+        if ($e->getAwsErrorMessage() !== null) {
+            $msg .= PHP_EOL . 'AWS error message: ' . $e->getAwsErrorMessage();
+        }
+        if ($e->getAwsErrorType() !== null) {
+            $msg .= PHP_EOL . 'AWS error type: ' . $e->getAwsErrorType();
+        }
+        if ($e->getAwsErrorCode() !== null) {
+            $msg .= PHP_EOL . 'AWS error code: ' . $e->getAwsErrorCode();
+        }
+
+        throw new QueueSendException($msg);
     }
 }
